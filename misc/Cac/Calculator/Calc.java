b8b3e599efe17258d598afc24352ccc4ad5db8cc
JK <kultscher58@gmail.com>
2023-03-09 20:05:30 +0100

    second commit

--- misc/Cac/Calculator/Calc.java
+++ misc/Cac/Calculator/Calc.java
@@ -15,33 +15,22 @@ public class Calc {
     /**
      * Generates random Number
      *
-     * @param x lower bound
-     * @param y upper bound
-     * @return random number between x and y
+     * @param lowerBound lower bound
+     * @param upperBound upper bound
+     * @return pseudo-random number between lowerBound and upperBound
      */
-    public static int rng(int x, int y) {
-        return ThreadLocalRandom.current().nextInt(x, y + 1);
-    }
-
-    /**
-     * Raises x to the power of y
-     *
-     * @param x base
-     * @param y exponent
-     * @return x to the y-th power
-     */
-    public static double exp(int x, int y) {
-        return Math.pow(x, y);
+    public static int rng(int lowerBound, int upperBound) {
+        return ThreadLocalRandom.current().nextInt(lowerBound, upperBound + 1);
     }
 
     /**
      * Converts integers in the decimal system to the binary system
      *
-     * @param x integer in the decimal system
-     * @return x in the binary system
+     * @param decimalNumber integer in the decimal system
+     * @return decimalNumber in the binary system
      */
-    public static long Binary(int x) {
-        int b = x;
+    public static long Binary(int decimalNumber) {
+        int b = decimalNumber;
         int d = 0;
         while (b > 0) {
             b = b / 2;
@@ -50,78 +39,87 @@ public class Calc {
         long[] binaryArray = new long[d];
         String[] binaryStringArray = new String[d];
         for (int j = d - 1; j >= 0; j--) {
-            binaryArray[j] = x % 2;
+            binaryArray[j] = decimalNumber % 2;
             binaryStringArray[j] = binaryArray[j] + "";
-            x = x / 2;
+            decimalNumber = decimalNumber / 2;
         }
         String binaryString = String.join("", binaryStringArray);
         return Long.parseLong(binaryString);
     }
 
     /**
-     * Converts s in the binary system to a long integer in the decimal system
+     * Converts binaryNumberString to a long integer in the decimal system
      *
-     * @param s a number in the binary system as a string
-     * @return s in the decimal system
+     * @param binaryNumberString a number in the binary system as a string
+     * @return binaryNumberString in the decimal system
      */
-    public static long decimal(String s) {
-        int v = s.length();
-        int[] bin = new int[v];
-        String[] nums = new String[v];
+    public static long decimal(String binaryNumberString) {
+        int digitsOfBinaryNumber = binaryNumberString.length();
+        int[] binaryNumberArray = new int[digitsOfBinaryNumber];
+        String[] binaryStringArray = new String[digitsOfBinaryNumber];
         long n = 0;
-        for (int i = 0; i < v; i++) {
-            nums[i] = s.substring(i, i + 1);
+        for (int i = 0; i < digitsOfBinaryNumber; i++) {
+            binaryStringArray[i] = binaryNumberString.substring(i, i + 1);
         }
-        for (int i = 0; i < v; i++) {
-            bin[i] = Integer.parseInt(nums[i]);
+        for (int i = 0; i < digitsOfBinaryNumber; i++) {
+            binaryNumberArray[i] = Integer.parseInt(binaryStringArray[i]);
         }
-        int v2 = v - 1;
-        for (int i = 0; i < v; i++) {
-            n = n + Math.round(Math.pow(2, v2)) * bin[i];
+        int v2 = digitsOfBinaryNumber - 1;
+        for (int i = 0; i < digitsOfBinaryNumber; i++) {
+            n = n + Math.round(Math.pow(2, v2)) * binaryNumberArray[i];
             v2--;
         }
         return n;
     }
 
     /**
-     * Converts x in the binary system to a long integer in the decimal system
+     * Converts binaryNumber to a long integer in the decimal system
      *
-     * @param x a number in the binary system
-     * @return x in the decimal system
+     * @param binaryNumber a number in the binary system
+     * @return binaryNumber in the decimal system
      */
-    public static long decimal(int x) {
-        String number = Integer.toString(x);
-        int v = number.length();
-        int[] bin = new int[v];
-        String[] nums = new String[v];
-        long n = 0;
-        for (int i = 0; i < v; i++) {
-            nums[i] = number.substring(i, i + 1);
+    public static long decimal(int binaryNumber) {
+        String number = Integer.toString(binaryNumber);
+        int binaryNumberDigits = number.length();
+        int[] binaryNumberIntArray = new int[binaryNumberDigits];
+        String[] binaryNumberStringArray = new String[binaryNumberDigits];
+        long decimalReturnValue = 0;
+        for (int i = 0; i < binaryNumberDigits; i++) {
+            binaryNumberStringArray[i] = number.substring(i, i + 1);
         }
-        for (int i = 0; i < v; i++) {
-            bin[i] = Integer.parseInt(nums[i]);
+        for (int i = 0; i < binaryNumberDigits; i++) {
+            binaryNumberIntArray[i] = Integer.parseInt(binaryNumberStringArray[i]);
         }
-        int v2 = v - 1;
-        for (int i = 0; i < v; i++) {
-            n = n + Math.round(Math.pow(2, v2)) * bin[i];
+        int v2 = binaryNumberDigits - 1;
+        for (int i = 0; i < binaryNumberDigits; i++) {
+            decimalReturnValue = decimalReturnValue + Math.round(Math.pow(2, v2)) * binaryNumberIntArray[i];
             v2--;
         }
-        return n;
+        return decimalReturnValue;
     }
 
-    
-    public static boolean isPrimeNumber( int n ) {
-        int b;
-        for ( int i = 2 ; i < n ; i++ ) {
-            b = n % i;
-            if ( b == 0 ) return false;
+    /**
+     * Checks if inputNumber is a prime number and if it is returns 'true' otherwise it will return 'false'.
+     * @param inputNumber
+     * @return 'true' if inputNumber is a prime number and 'false' if otherwise.
+     */
+    public static boolean isPrimeNumber( int inputNumber ) {
+        int remainderOfFraction;
+        for ( int i = 2 ; i < inputNumber ; i++ ) {
+            remainderOfFraction = inputNumber % i;
+            if ( remainderOfFraction == 0 ) return false;
         }
         return true;
     }
-    
-    public static boolean isEven(int n){
-        int e = n % 2;
-        return e == 0;
+
+    /**
+     * Checks if inputNumber is even and returns 'true' if it is and 'false' if otherwise.
+     * @param inputNumber
+     * @return returns wether inputNumber is even or odd.
+     */
+    public static boolean isEven(int inputNumber){
+        int remainderOfFraction = inputNumber % 2;
+        return remainderOfFraction == 0;
     }
 }
 
