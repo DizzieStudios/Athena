a0b39101a351b2d23621eb060629b7da829a3ebe
JK <kultscher58@gmail.com>
2023-03-29 15:13:10 +0200

    commit

--- misc/Misc_Test.java
+++
@@ -1,8 +0,0 @@
-
-import Tools.NumberTools;
-class Misc_Test {
-    public static void main(String[] args) {
-        System.out.println(NumberTools.roundToInt(3.5));
-        System.out.println(NumberTools.roundToInt(3.4));
-    }
-}
