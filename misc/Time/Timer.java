a28015fee79e20083d54539222b6fde923b95e9f
JK <kultscher58@gmail.com>
2023-04-17 09:03:33 +0200

    commit

--- misc/Time/Timer.java
+++ misc/Time/Timer.java
@@ -5,7 +5,34 @@ public class Timer {
     private int seconds;
     private int minutes;
     public Timer(){
+        milliseconds = 0;
+        seconds = 0;
+        minutes = 0;
+        exe();
+    }
+    public void exe(){
+        sleep();
+        ++milliseconds;
+        if ( milliseconds > 999){
+            ++seconds;
+            milliseconds = 0;
+        }
+        if ( seconds > 59){
+            ++minutes;
+            seconds = 0;
+        }
+    }
 
+    public int getMilliseconds() {
+        return milliseconds;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public int getMinutes() {
+        return minutes;
     }
 
     private void sleep(){
