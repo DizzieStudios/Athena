e17af0a7ed593ba81d3ac0921ab2ba099a08f371
JK <kultscher58@gmail.com>
2023-04-18 11:23:07 +0200

    commit

--- misc/Test.java
+++ misc/Test.java
@@ -1,16 +1,8 @@
+
 import Tools.NumberTools;
 class Test {
 
     public static void main(String[] args) {
-        System.out.println(NumberTools.roundToDouble(3.5,2));
-        System.out.println(NumberTools.roundToDouble(3.6,2));
-        System.out.println(NumberTools.roundToDouble(3.2,2));
-        System.out.println(NumberTools.roundToDouble(3.1,3));
-        System.out.println(NumberTools.roundToDouble(3.7,3));
-        System.out.println(NumberTools.roundToDouble(3.534,2));
-        System.out.println(NumberTools.roundToDouble(3.636,2));
-        System.out.println(NumberTools.roundToDouble(3.231,2));
-        System.out.println(NumberTools.roundToDouble(3.137,3));
-        System.out.println(NumberTools.roundToDouble(3.796,3));
+
     }
 }
